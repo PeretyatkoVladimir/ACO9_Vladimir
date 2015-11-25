14a22819fb621ff11ef174b41cf594f68202db2e
PeretyatkoVladimir <voc@ukr.net>
2015-11-25 17:00:43 +0200

    Update Circus.java

--- src/hw/week3/circus/Circus.java
+++ src/hw/week3/circus/Circus.java
@@ -6,5 +6,46 @@ public class Circus {
   private ArrayList<Stuff> artists;
   private Location location;
   
-
+  // task 1
+  public String viewArtists() {
+    String allArtists = "";
+    for (Stuff a : artists) {
+      allArtists += a + "/n";
+    }
+    return allArtists;
+  }
+  
+  // task 4
+  public String goPlay(Stuff artist) {
+    return artist.viewPlay();
+  }
+  
+  // task 5
+  public String viewArtistsActed() {
+    String allArtistsActed = "";
+    for (Stuff a : artists) {
+      allArtistsActed += a + ", acted - " + a.getActed() + " times. /n";
+    }
+    return allArtistsActed;
+  } 
+  
+  // task 6
+  public String viewArtistsSalary() {
+    String allArtistsSalary = "";
+    for (Stuff a : artists) {
+      allArtistsActed += a + ", salary - " + a.getSalary() + "/n";
+    }
+    return allArtistsSalary;
+  } 
+  
+  // task 7
+  public void changeLocation() {
+    
+  } 
+  
+  // task 7.1
+  public void viewCurentLocation() {
+    
+  }
+  
 }
