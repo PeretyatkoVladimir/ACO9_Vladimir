48c656f4ea0ccde0a0a063c9777ae655292dfad3
PeretyatkoVladimir <voc@ukr.net>
2015-11-27 21:07:02 +0200

    new changes

--- src/hw/week3/circus/model/Acrobat.java
+++ src/hw/week3/circus/model/Acrobat.java
@@ -2,9 +2,10 @@ package hw.week3.circus.model;
 
 import utils.DataInput;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Acrobat extends Artist {
+public class Acrobat extends Artist implements Comparable {
 
     int drunks;
 
@@ -28,6 +29,34 @@ public class Acrobat extends Artist {
         return "Acrobat{" +
                 "name='" + super.getName()  +
                 '}';
+    }
+
+    public int compareTo(Object anObject) {
+
+        Acrobat anAcrobat = (Acrobat)anObject;
+        if(this.drunks < anAcrobat.drunks) {
+            return -1;
+        } else if(this.drunks > anAcrobat.drunks) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static Acrobat findDrunkestAcrobat(ArrayList<Acrobat> acrobats) {
+
+        if (acrobats.size() == 0)  {
+            return null;
+        }
+
+        Acrobat max = acrobats.get(0);
+
+        for (Acrobat a : acrobats) {
+            if (max.compareTo(a) > 0) {
+                max = a;
+            }
+        }
+        return max;
     }
 
     @Override
