d9a7aff34c721a0f31e4442cc58afd3ea001adc5
PeretyatkoVladimir <voc@ukr.net>
2015-12-03 20:51:24 +0200

    new changes

--- src/hw/week4/queue/LinkedQueue.java
+++ src/hw/week4/queue/LinkedQueue.java
@@ -1,6 +1,12 @@
 package hw.week4.queue;
 
+import hw.week4.LinkedCell;
+
 public class LinkedQueue implements IQueue {
+
+    private LinkedCell head;
+    private LinkedCell tail;
+
     @Override
     public void enq(Object object) {
 
